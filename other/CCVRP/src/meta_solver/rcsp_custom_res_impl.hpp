eb951448c8622a4e2989bfd7df8f7c3dce8fd3a7
Artur Alves Pessoa <artur.a.pessoa@gmail.com>
2025-12-05 11:34:11 -0300

    Removing useless C++ constructors in the meta-solver demo

--- other/CCVRP/src/meta_solver/rcsp_custom_res_impl.hpp
+++ other/CCVRP/src/meta_solver/rcsp_custom_res_impl.hpp
@@ -15,93 +15,74 @@
 namespace rcsp_custom_res
 {
 
-struct CustomResArcParameters
-{
-    double t;
-    double w;
-
-    CustomResArcParameters() { w = t = 0.0; }
-
-    CustomResArcParameters(double w_, double t_)
+    struct CustomResArcParameters
     {
-        w = w_;
-        t = t_;
-    }
-};
+        double t;
+        double w;
+    };
 
-struct CustomResVertexParameters
-{
-};
-
-struct CustomResConstParameters
-{
-    double Wmax;
-    double Tmax;
-
-    CustomResConstParameters(double w, double t)
+    struct CustomResVertexParameters
     {
-        Wmax = w;
-        Tmax = t;
-    }
-    CustomResConstParameters()
+    };
+
+    struct CustomResConstParameters
     {
-        Wmax = 0.0;
-        Tmax = 0.0;
-    }
-};
+        double Wmax;
+        double Tmax;
+    };
 
-struct CustomResParameters
-{
-    std::vector<double> t;
-    std::vector<double> w;
-    double Wmax;
-    double Tmax;
+    struct CustomResParameters
+    {
+        std::vector<double> t;
+        std::vector<double> w;
+        double Wmax;
+        double Tmax;
 
-    void setDimensions(int n, int m);
-    void setArcParameter(int a, const CustomResArcParameters &value);
-    void setVertexParameter(int v, const CustomResVertexParameters &value);
-    void setConstParameter(const CustomResConstParameters &value);
-};
+        void setDimensions(int n, int m);
+        void setArcParameter(int a, const CustomResArcParameters &value);
+        void setVertexParameter(int v, const CustomResVertexParameters &value);
+        void setConstParameter(const CustomResConstParameters &value);
+    };
 
-struct ForwardState
-{
-    double S;
-    double T;
-};
+    struct ForwardState
+    {
+        double S;
+        double T;
+    };
 
-struct BackwardState
-{
-    double S;
-    double W;
-};
+    struct BackwardState
+    {
+        double S;
+        double W;
+    };
 
-bool symmetric(const CustomResParameters &Rcc);
+    bool symmetric(const CustomResParameters &Rcc);
 
-void initState(const CustomResParameters &Rcc, ForwardState &state);
+    void initState(const CustomResParameters &Rcc, ForwardState &state);
 
-void initState(const CustomResParameters &Rcc, BackwardState &state);
+    void initState(const CustomResParameters &Rcc, BackwardState &state);
 
-double extendToVertex(const CustomResParameters &Rcc, ForwardState &state, int v);
+    double extendToVertex(const CustomResParameters &Rcc, ForwardState &state, int v);
 
-double extendAlongArc(const CustomResParameters &Rcc, ForwardState &state, int a);
+    double extendAlongArc(const CustomResParameters &Rcc, ForwardState &state, int a);
 
-double extendToVertex(const CustomResParameters &Rcc, BackwardState &state, int v);
+    double extendToVertex(const CustomResParameters &Rcc, BackwardState &state, int v);
 
-double extendAlongArc(const CustomResParameters &Rcc, BackwardState &state, int a);
+    double extendAlongArc(const CustomResParameters &Rcc, BackwardState &state, int a);
 
-double dominationCost(const CustomResParameters &Rcc, int v, const ForwardState &dominating,
-                      const ForwardState &dominated);
+    double dominationCost(const CustomResParameters &Rcc, int v, const ForwardState &dominating,
+                          const ForwardState &dominated);
 
-double dominationCost(const CustomResParameters &Rcc, int v, const BackwardState &dominating,
-                      const BackwardState &dominated);
+    double dominationCost(const CustomResParameters &Rcc, int v, const BackwardState &dominating,
+                          const BackwardState &dominated);
 
-double concatenationCost(const CustomResParameters &Rcc, int v, const ForwardState &fwd,
-                         const BackwardState &bwd);
+    double concatenationCost(const CustomResParameters &Rcc, int v, const ForwardState &fwd,
+                             const BackwardState &bwd);
 
-double concatenationCost(const CustomResParameters &Rcc, int v, const ForwardState &fwd,
-                         const ForwardState &bwd);
+    double concatenationCost(const CustomResParameters &Rcc, int v, const ForwardState &fwd,
+                             const ForwardState &bwd);
 
-bool isCostResource();
+    bool isCostResource();
 
 } // namespace rcsp_custom_res
 
