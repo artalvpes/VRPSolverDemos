651084e8bdc6363ffd3455534b7ad8af4de49759
Artur Alves Pessoa <artur.a.pessoa@gmail.com>
2025-12-07 18:59:55 -0300

    Add shell script and batch files for benchmark runs

---
+++ F.sh
@@ -0,0 +1,4 @@
+# Running all instances of the sets F
+julia src/run data/F/F-n45-k4.vrp -m 4 -M 4 -u 725 &> out/F-n45-k4.vrp
+julia src/run data/F/F-n72-k4.vrp -m 4 -M 4 -u 238 &> out/F-n72-k4.vrp
+julia src/run data/F/F-n135-k7.vrp -m 7 -M 7 -u 1163 &> out/F-n135-k7.vrp
