780511fabac9bfc14f4fbd9b50c1732f1f6bd0cb
Teobaldo Bulhões <tbulhoes@ci.ufpb.br>
2026-04-24 16:25:13 -0300

    removing disabled functions and changing vrpsolver branch

--- CVRP/src/CVRPSolverDemo.jl
+++ CVRP/src/CVRPSolverDemo.jl
@@ -54,9 +54,6 @@ function parse_commandline(args_array::Vector{String}, appfolder::String)
         "--edge_cuts", "-e"
         help = "Use edge cuts via cut callback."
         action = :store_true
-        "--strong_kpath_cuts", "-k"
-        help = "Use strong k-path cuts."
-        action = :store_true
         "--cluster_branching", "-B"
         help = "Cluster branching parameter value (0 = disabled)"
         arg_type = Float64
